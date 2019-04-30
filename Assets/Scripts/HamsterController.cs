afcff8a10963adf3f2342907b3c01fd2c87c8690
piotrek <p.gajdzica@labmatic.com>
2019-04-30 11:55:00 +0200

    Main scene and basic movement
    Added MainScene with hamster and example obstacles. Implemented HamsterController with gravity and vertical movement (space or up key)

---
+++ Assets/Scripts/HamsterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HamsterController : MonoBehaviour
+{
+    protected Vector2 velocity;
+    protected float gravityModifier = 0.1f;
+    protected float flightModifier = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Vector2.zero;
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+        Physics2D.gravity = new Vector2(0, -1.0F);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        float vertical = Input.GetAxis("Vertical");
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        velocity.y += vertical * Time.deltaTime;
+        Vector2 position = transform.position;
+        position += velocity;
+        transform.position = position;
+    }
+}
