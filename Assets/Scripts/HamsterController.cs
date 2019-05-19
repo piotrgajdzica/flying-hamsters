ebbfc00646c6c0a493d1bf67c739b8a69104a57f
piotrek <p.gajdzica@labmatic.com>
2019-05-19 12:45:05 +0200

    Collision detection

--- Assets/Scripts/HamsterController.cs
+++ Assets/Scripts/HamsterController.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class HamsterController : MonoBehaviour
 {
-    protected Vector2 velocity;
-    protected float gravityModifier = 0.1f;
-    protected float flightModifier = 1f;
+    public Rigidbody2D rb;
+    //protected float gravityModifier = 0.1f;
+    protected float flightModifier = 10f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        velocity = Vector2.zero;
+        rb = GetComponent<Rigidbody2D>();
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 30;
         Physics2D.gravity = new Vector2(0, -1.0F);
@@ -22,10 +22,17 @@ public class HamsterController : MonoBehaviour
     void Update()
     {   
         float vertical = Input.GetAxis("Vertical");
-        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
-        velocity.y += vertical * Time.deltaTime;
-        Vector2 position = transform.position;
-        position += velocity;
-        transform.position = position;
+        //velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        Vector2 velocity = rb.velocity;
+        velocity.y += vertical * Time.deltaTime * flightModifier;
+        rb.velocity = velocity;
+
+
+        Vector2 v = rb.velocity;
+        float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        /*Vector2 position = rb.position;
+        position += rb.velocity;
+        transform.position = position;*/
     }
 }
