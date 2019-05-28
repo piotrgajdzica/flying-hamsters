70b5c17777893ae46571ccf29d0c42da1d389c5d
piotrek <p.gajdzica@labmatic.com>
2019-05-28 10:53:42 +0200

    Added energy bar abd points text

---
+++ Assets/Scripts/EnergyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyController : MonoBehaviour
+{
+
+    public Slider energySlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        energySlider.value = HamsterController.instance.currentEnergy;
+    }
+}
---
+++ Assets/Scripts/PointsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // The namespace for the UI stuff.
+
+public class PointsController : MonoBehaviour
+{
+    private HamsterController hamsterController;
+    private Text m_TextComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        // Get a reference to the text component
+        m_TextComponent = GetComponent<Text>();
+
+        m_TextComponent.text = "This is the new text.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        m_TextComponent.text = Mathf.Round(HamsterController.instance.rb2d.position.x) + " m";
+    }
+}
